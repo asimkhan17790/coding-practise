cfd83a6b84efac9b9e0b3f7bdf714887910429f1
Asim Khan <asim.khan17790@gmail.com>
2021-05-27 02:04:24 -0400

    Test is a relationship

--- coding-practise/src/main/java/com/leetcode/Test.java
+++ coding-practise/src/main/java/com/leetcode/Test.java
@@ -3,6 +3,12 @@ package com.leetcode;
 public class Test {
     public static void main(String ...s) {
         System.out.println("Hi There. I am Asim Khan");
-        
+        C c = new C();
+        System.out.println(c instanceof A);
     }
+    static class A{}
+    static class B extends A{}
+    static class C extends B{}    
 }
+
+
