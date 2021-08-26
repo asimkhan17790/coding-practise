12a5325ea39e3be288d7c9fecca1daf1fa759f8d
Asim Khan <asim.khan17790@gmail.com>
2021-08-26 01:56:29 -0400

    permutaiton logic 1

---
+++ coding-practise/src/main/java/com/leetcode/FindStringinBiggerString.java
@@ -0,0 +1,68 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// give 2 strings s:abbc and b:cbabadcbbabbcbabaabccbabc find index all permutations of b in s. The permutation should be contiguous.
+public class FindStringinBiggerString {   
+
+    public static void main(String[] args) {
+        System.out.println(findLocations("cbabadcbbabbcbabaabccbabc", "abbc")); 
+    }
+
+    private static List<Integer> findLocations (String b, String s) {
+        List<Integer> indexList = new ArrayList<>();
+
+        // logic 1 - start
+        Set<Character> set = new HashSet<>();
+        for (int i=0;i<s.length();i++){
+            set.add(s.charAt(i));
+        }
+
+        for (int i = 0; i<= b.length()-s.length();i++) {
+               if (set.contains(b.charAt(i))){
+                   if (isPermutation(s, b.substring(i, i+s.length()))){
+                    System.out.println(i);    
+                    indexList.add(i);
+                   }
+               }
+        }
+
+        return indexList;       
+
+    }
+
+    private static boolean isPermutation (String s1, String s2) {
+
+        System.out.println("String 1 =" +s1);
+        System.out.println("String 2 =" +s2);
+
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        char [] ss1 = s1.toCharArray();
+        char [] ss2 = s2.toCharArray();
+
+        Arrays.sort(ss1);
+        Arrays.sort(ss2);
+
+        for (int i = 0;i < ss1.length;i++) {
+            if (ss1[i]!=ss2[i]) return false;
+        }
+        return true;
+    }
+
+    // logic 1 -- end
+
+    // logic 2 - start
+
+    //logic 2 - end
+
+}
+
+
+
+
