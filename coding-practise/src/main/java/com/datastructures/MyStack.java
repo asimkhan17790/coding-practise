5a96808b4f055f765e970b1e5515d3f5c0e997d8
Asim Khan <asim.khan17790@gmail.com>
2021-08-30 03:36:34 -0400

    Implementation of stack

---
+++ coding-practise/src/main/java/com/datastructures/MyStack.java
@@ -0,0 +1,41 @@
+package com.datastructures;
+
+import java.util.EmptyStackException;
+// Implementation  of Stack Data structure using a LinkedList
+public class MyStack<T> {
+
+    private static class StackNode<T> {
+        private T data;
+        private StackNode<T> next;
+
+        public StackNode(T data){
+            this.data = data;
+        }
+    }
+
+    private StackNode<T> top;
+
+
+    public T pop() {
+        if (top == null) throw new EmptyStackException();
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+    
+    public void push (T item) {
+
+        StackNode<T> t = new StackNode<T>(item);
+        t.next = top;
+        top = t;
+    }
+
+    public T peek() {
+        if (top == null) throw new EmptyStackException();
+        return top.data;
+    }
+
+    public Boolean isEmpty() {
+        return top == null;
+    }
+}
