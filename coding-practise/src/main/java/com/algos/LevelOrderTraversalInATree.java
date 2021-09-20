a13dfb3a60a56d181013658978eaa4418e73aeaa
Asim Khan <asim.khan17790@gmail.com>
2021-09-20 01:36:57 -0400

    Inorder preorder and level Order

--- coding-practise/src/main/java/com/algos/LevelOrderTraversalInATree.java
+++ coding-practise/src/main/java/com/algos/LevelOrderTraversalInATree.java
@@ -12,7 +12,37 @@ public class LevelOrderTraversalInATree {
         t.root.right = new BinaryTreeNode(3);
 
         t.root.left.left = new BinaryTreeNode(4);
+        System.out.print("Level Order: ");
         levelOrderPrint(t.root);
+        System.out.println();
+        System.out.print("Inorder: ");
+
+        inOrderTraversal(t.root);
+        System.out.println();
+        System.out.print("Preorder: ");
+        preOrderTraversal(t.root);
+        System.out.println();
+        System.out.print("Posteorder: ");
+        postOrderTraversal(t.root);
+
+    }
+    static void inOrderTraversal(BinaryTreeNode node){
+        if (node == null) return;
+        inOrderTraversal(node.left);
+        System.out.print(node.key + "  ");
+        inOrderTraversal(node.right);
+    }
+    static void preOrderTraversal(BinaryTreeNode node){
+        if (node == null) return;
+        System.out.print(node.key + "  ");
+        preOrderTraversal(node.left);
+        preOrderTraversal(node.right);
+    }
+    static void postOrderTraversal(BinaryTreeNode node){
+        if (node == null) return;
+        postOrderTraversal(node.left);
+        postOrderTraversal(node.right);
+        System.out.print(node.key + "  ");        
     }
 
     public static void levelOrderPrint(BinaryTreeNode root) {
