14709a6ee222ff25734e529fda46a2d4c70e643c
Asim Khan <asim.khan17790@gmail.com>
2021-09-26 21:15:30 -0400

    Beautiful Sequence Program

---
+++ coding-practise/src/main/java/com/algos/BeautifulSequence.java
@@ -0,0 +1,19 @@
+package com.algos;
+
+
+//Given a string containing only characters T and H (COINS) create a beautiful sequence with minimum flips
+// Beautiful sequence: All H should be before T
+// The final sequence can be all H or all T as well, as it is also condifered as a beautiful sequence.
+public class BeautifulSequence {
+    public static void main(String[] args) {
+        System.out.println(minNoOfFlips("THHT"));
+    }
+
+    static int minNoOfFlips(String sequence) {
+        int count = 0;
+
+
+        return count;
+    }
+    
+}
