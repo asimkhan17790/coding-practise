89809309acdea593d615605d896a7cc9d68c4a3a
Asim Khan <asim.khan17790@gmail.com>
2021-10-03 18:39:34 -0400

    Beautiful sequence

--- coding-practise/src/main/java/com/algos/BeautifulSequence.java
+++ coding-practise/src/main/java/com/algos/BeautifulSequence.java
@@ -6,14 +6,22 @@ package com.algos;
 // The final sequence can be all H or all T as well, as it is also condifered as a beautiful sequence.
 public class BeautifulSequence {
     public static void main(String[] args) {
-        System.out.println(minNoOfFlips("THHT"));
+        System.out.println("Min number of flipse required to make it a Beautiful sequence: " + minNoOfFlips("THHT"));
     }
 
     static int minNoOfFlips(String sequence) {
-        int count = 0;
-
-
-        return count;
+        int tails = 0;
+        int flips = 0;
+        for (int i = 0; i < sequence.length(); i++){
+            if (sequence.charAt(i)=='T') {
+                tails++;
+            }
+            if(sequence.charAt(i)=='H'){
+                flips++;
+            }
+            flips = Math.min(tails,flips);
+        }
+        return flips;
     }
     
 }
