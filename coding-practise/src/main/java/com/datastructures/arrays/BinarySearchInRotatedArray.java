4733af9d8717742bd4fef79c1187bfa8fd9736a3
Asim Khan <asim.khan17790@gmail.com>
2021-11-01 01:55:12 -0400

    rotated binary search amzn2

--- coding-practise/src/main/java/com/datastructures/arrays/BinarySearchInRotatedArray.java
+++ coding-practise/src/main/java/com/datastructures/arrays/BinarySearchInRotatedArray.java
@@ -3,9 +3,9 @@ package com.datastructures.arrays;
 public class BinarySearchInRotatedArray {
     
     public static void main(String[] args) {
-        int arr1[] = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+        int arr1[] = { 5, 6, 7, 8, 9, 10, -1, 0, 1, 2, 3 };
         int n = arr1.length;
-        int key = 3;
+        int key = -1;
         System.out.println("Index of the element is : "
                            + pivotedBinarySearch(arr1, n, key));
     }
@@ -29,18 +29,23 @@ public class BinarySearchInRotatedArray {
     }
 
     static int findPivot(int ar[], int low, int high){
-        if (high<low) 
-            return -1;
-        if (high == low) 
-            return low;
-        int mid = (low + high)/2;
-        if (mid< high && ar[mid]>ar[mid+1]) 
-            return mid;
-        if (mid>low && ar[mid]<ar[mid-1]) 
-            return mid-1;
-        if(ar[low]>=ar[mid])
-            return findPivot(ar, low, mid-1);
-        return findPivot(ar, mid + 1, high);
+        
+        if (ar[low]<ar[high]) return -1;
+
+        while(low<=high) {
+            int mid = (low+high)/2;
+            if(ar[mid]>ar[mid+1]){
+                return mid;
+            }
+            else {
+                if(ar[mid]<ar[low]){
+                    high = mid-1;
+                }else {
+                    low = mid+1;
+                }
+            }
+        }
+        return -1;
     }
     static int binarySearch(int ar[], int left, int right, int key ){
         if (left > right) return -1;
