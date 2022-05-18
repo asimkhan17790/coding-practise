a53aac638a45cad719acd97d47b00508450051be
Asim Khan <asim.khan17790@gmail.com>
2022-05-18 01:15:40 -0400

    Subset sum

---
+++ coding-practise/src/main/java/com/algos/dynamicProgramming/knapsack/EqualSubsetSumPartitionBottomUp.java
@@ -0,0 +1,52 @@
+package com.algos.dynamicProgramming.knapsack;
+
+import java.util.Arrays;
+
+public class EqualSubsetSumPartitionBottomUp {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4};
+        System.out.println(canPartition(num));
+        num = new int[]{1, 1, 3, 4, 7};
+        System.out.println(canPartition(num));
+        num = new int[]{2, 3, 4, 6};
+        System.out.println(canPartition(num));
+    }
+
+
+    public static boolean canPartition(int [] num) {
+
+        int sum = Arrays.stream(num).sum();
+
+        if (sum%2!=0){
+            return false;
+        }
+
+        sum /= 2;
+
+        boolean dp [][] = new boolean[num.length][sum+1];
+
+        for (int i = 0;i<num.length;i++){
+            dp[i][0] = true;
+
+        }
+
+        for (int i = 0;i<=sum;i++){
+            dp[0][i] = ((num[0] == i) ? true:false);
+        }
+
+
+        for (int i = 1;i<num.length;i++) {
+            for (int s = 1; s <= sum; s++) {
+                if (dp[i-1][s]) {
+                    dp[i][s] = dp[i-1][s];
+                } else if (num[i]<=s) {
+                    dp[i][s] =  dp[i-1][s - num[i]];
+                }
+            }
+        }
+
+    return dp [num.length-1][sum];
+    }
+             
+
+}
---
+++ coding-practise/src/main/java/com/algos/dynamicProgramming/knapsack/SubsetSumBottomUp.java
@@ -0,0 +1,37 @@
+package com.algos.dynamicProgramming.knapsack;
+
+public class SubsetSumBottomUp {
+
+    public static void main(String[] args) {        
+            int[] num = { 1, 2, 3, 7 };
+            System.out.println(canPartition(num, 6));
+            num = new int[] { 1, 2, 7, 1, 5 };
+            System.out.println(canPartition(num, 10));
+            num = new int[] { 1, 3, 4, 8 };
+            System.out.println(canPartition(num, 6));
+    }
+
+
+    public static boolean canPartition (int [] num, int SUM) {
+
+        boolean dp [][] = new boolean [num.length][SUM+1];
+        for (int i = 0; i < num.length;i++) {
+            dp [i][0] = true;
+        }
+        for (int s = 0;s <= SUM;s++) {
+            dp[0][s] = (num[0]==s ? true:false);
+        }
+
+        for (int i = 1;i<num.length;i++) {
+            for (int s=1; s <= SUM; s++){
+                if (dp[i-1][s]) {
+                    dp[i][s] = dp [i-1][s];
+                }
+                else if (num[i]<=s) {
+                    dp [i][s] = dp[i-1][s-num[i]];
+                }
+            }
+        }
+        return dp[num.length-1][SUM];
+    }    
+}
