1163838b7553f1f43b7db2fb816eeff79e42ee58
Asim Khan <asim.khan17790@gmail.com>
2022-05-23 02:49:36 -0400

    min coin change educative using 2d array

--- coding-practise/src/main/java/com/algos/dynamicProgramming/educativeIO/pattern2UnboundedKnapsack/MinimumCoinChange.java
+++ coding-practise/src/main/java/com/algos/dynamicProgramming/educativeIO/pattern2UnboundedKnapsack/MinimumCoinChange.java
@@ -11,7 +11,51 @@ public class MinimumCoinChange {
         denominations = new int[]{3, 5};
         System.out.println(cc.countChange(denominations, 7));
 
+
         
+        System.out.println("Coin Change: Educative.io Algo");
+        denominations =  new int[]{1, 2, 3};
+        System.out.println(cc.countChange2DArray(denominations, 5));
+        System.out.println(cc.countChange2DArray(denominations, 11));
+        System.out.println(cc.countChange2DArray(denominations, 7));
+        denominations = new int[]{3, 5};
+        System.out.println(cc.countChange2DArray(denominations, 7));
+
+        
+    }
+
+    // Educative.io Algorithm
+    private int countChange2DArray (int [] denom, int TARGET) {
+        int dp [][] = new int [denom.length][TARGET+1];
+
+        for(int i=0; i < denom.length; i++){
+            for(int j=0; j <= TARGET; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }                
+        } 
+
+        for (int i = 0; i < denom.length; i++) {           
+                dp[i][0] = 0;            
+        }
+
+        for (int i = 0; i < denom.length; i++){
+            for (int sum = 1; sum <= TARGET; sum++){  
+
+                //exclude
+                if (i > 0) {
+                    dp[i][sum] = dp[i-1][sum];
+                }                    
+                //include
+                if (denom[i]<=sum){                    
+                    if (dp[i][sum-denom[i]] != Integer.MAX_VALUE){
+                        dp[i][sum] = Math.min(dp[i][sum], 1 + dp[i][sum-denom[i]]);
+                    }
+                    
+                }                 
+            }
+        }
+
+        return (dp[denom.length-1][TARGET] == Integer.MAX_VALUE ? -1 : dp[denom.length-1][TARGET]);
     }
 
 
