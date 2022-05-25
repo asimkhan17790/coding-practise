10cf39aa496002b10634f8a002e8dd715e8250c4
Asim Khan <asim.khan17790@gmail.com>
2022-05-25 01:40:57 -0400

    min jumps

---
+++ coding-practise/src/main/java/com/algos/dynamicProgramming/educativeIO/pattern3FibonacciNumbers/MinimumJumpsToReachEndBottomUp.java
@@ -0,0 +1,32 @@
+package com.algos.dynamicProgramming.educativeIO.pattern3FibonacciNumbers;
+
+import java.util.Arrays;
+
+public class MinimumJumpsToReachEndBottomUp {
+    public static void main(String[] args) {
+        MinimumJumpsToReachEndBottomUp aj = new MinimumJumpsToReachEndBottomUp();
+        int[] jumps = {2, 1, 1, 1, 4};
+        System.out.println("Min Count: " + aj.countMinJumps(jumps));
+        jumps = new int[]{1, 1, 3, 6, 9, 3, 0, 1, 3};
+        System.out.println("Min Count: " + aj.countMinJumps(jumps));
+      }
+      public int countMinJumps(int jumps []){
+
+        int dp [] = new int[jumps.length];
+
+        for (int i = 1;i<jumps.length;i++){
+            dp[i]=Integer.MAX_VALUE;
+        }
+
+        for (int start = 0;start < jumps.length;start++){
+            for (int end=start+1;end<jumps.length && end<=start + jumps[start];end++){
+                dp[end] = Math.min(dp[end],dp[start]+1);
+            }
+        }
+        System.out.println(dp.length);
+        Arrays.stream(dp).forEach(s -> System.out.print(s +"|"));
+        System.out.println();
+        return dp[jumps.length-1];
+
+      }
+}
