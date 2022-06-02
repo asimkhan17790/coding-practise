540fd00cc98132f5bd8a6de332fa242bc59a46c3
Asim Khan <asim.khan17790@gmail.com>
2022-06-02 02:26:28 -0400

    ad

--- coding-practise/src/main/java/com/algos/dynamicProgramming/educativeIO/pattern5LongestCommonSubstring/ShortestCommonSupersequence.java
+++ coding-practise/src/main/java/com/algos/dynamicProgramming/educativeIO/pattern5LongestCommonSubstring/ShortestCommonSupersequence.java
@@ -5,6 +5,12 @@ public class ShortestCommonSupersequence {
         ShortestCommonSupersequence scs = new ShortestCommonSupersequence();
         System.out.println(scs.findSCSLength("abcf", "bdcf"));
         System.out.println(scs.findSCSLength("dynamic", "programming"));
+
+        // LEET CODE
+        System.out.println("--- Leetcode Solution Using Shortest Common Subsequence ---");
+        System.out.println(scs.getSCSLengthLeetcode("abcf", "bdcf"));
+        System.out.println(scs.getSCSLengthLeetcode("dynamic", "programming"));
+        
       }
 
       private int findSCSLength (String s1, String s2) {
@@ -30,4 +36,69 @@ public class ShortestCommonSupersequence {
 
         return dp[s1.length()][s2.length()];
       }
+      private int getSCSLengthLeetcode(String str1, String str2) {
+        return this.shortestCommonSupersequence(str1, str2).length();
+      }
+
+      // Leet Code Solution
+      public String shortestCommonSupersequence(String str1, String str2) {
+        
+        
+        if (str1.length() == 0 || str2.length() == 0){
+            return str1.length() == 0 ? str2:str1;
+        }
+        
+        int m = str1.length();
+        int n = str2.length();
+        int C[][] = new int [m+1][n+1];
+        
+        for (int i = 0;i<=m;i++){
+            C[i][0]=0;
+        }
+        for (int i = 0;i<=n;i++){
+            C[0][i] = 0;
+        }
+        
+        for (int i = 1;i<=m;i++){
+            for (int j = 1;j<=n;j++){                
+                if (str1.charAt(i-1) == str2.charAt(j-1)){
+                    C[i][j] = 1 + C[i-1][j-1];
+                }else {
+                    C[i][j] = Math.max(C[i][j-1],C[i-1][j]);
+                }
+            }
+        }
+        
+        int l = C[m][n];
+        StringBuilder s = new StringBuilder("");
+        
+        int i = m;
+        int j = n;
+        while (i > 0 && j > 0){
+            if (str1.charAt(i-1) == str2.charAt(j-1)){
+                s.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }else if (C[i-1][j] > C[i][j-1]){
+                s.append(str1.charAt(i-1));
+                i--;
+            } else {
+                s.append(str2.charAt(j-1));
+                j--;
+            }
+        }
+        while (i>0){
+            s.append(str1.charAt(i-1));
+            i--;
+        }
+        
+        while (j>0){
+            s.append(str2.charAt(j-1));
+            j--;
+        }     
+        
+        
+        return s.reverse().toString();
+        
+    }
 }
