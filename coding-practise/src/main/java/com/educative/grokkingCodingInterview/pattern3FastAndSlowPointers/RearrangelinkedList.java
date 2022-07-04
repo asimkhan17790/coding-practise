dde098ffc31c1d59cc421920e053b6111547796b
Asim Khan <asim.khan17790@gmail.com>
2022-07-03 23:31:46 -0400

    Rearranging list

--- coding-practise/src/main/java/com/educative/grokkingCodingInterview/pattern3FastAndSlowPointers/RearrangelinkedList.java
+++ coding-practise/src/main/java/com/educative/grokkingCodingInterview/pattern3FastAndSlowPointers/RearrangelinkedList.java
@@ -27,9 +27,41 @@ class ListNode {
 class RearrangeLinkedList {
 
     public static void reorder(ListNode head) {
-        // TODO: Write your code here
-    }
+        if (head == null || head.next == null)
+            return;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // slow is now pointing to the middle node
+        ListNode headSecondHalf = reverse(slow); // reverse the second half
+        ListNode headFirstHalf = head;
 
+        while(headFirstHalf!=null && headSecondHalf!=null){
+            ListNode temp = headFirstHalf.next;
+            headFirstHalf.next = headSecondHalf;
+            headFirstHalf=temp;
+
+            temp = headSecondHalf.next;
+            headSecondHalf.next=headFirstHalf;
+            headSecondHalf=temp;
+        }
+
+        if (headFirstHalf!=null){
+            headFirstHalf.next=null;
+        }
+    }
+    private static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
     public static void main(String[] args) {
         ListNode head = new ListNode(2);
         head.next = new ListNode(4);
