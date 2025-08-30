c4877b3a0fd85dc879940ab1d949869d9cde52d1
Asim Khan <asim.khan17790@gmail.com>
2025-08-29 20:22:56 -0400

    streamlined merge sort algo

--- coding-practise/src/main/java/com/algos/MergeSort.java
+++ coding-practise/src/main/java/com/algos/MergeSort.java
@@ -4,43 +4,49 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int ar[] = {2,4,5,7,1,2,3,6,56,23,36,12,87,12,2111};
-        mergeSort(ar, 0, ar.length-1);
-        Arrays.stream(ar).forEach(e->System.out.print(e + " "));
+        int ar[] = {2, 4, 5, 7, 1, 2, 3, 6, 56, 23, 36, 12, 87, 12, 2111};
+
+        int temp[] = new int[ar.length];
+        mergeSort(ar, 0, ar.length - 1, temp);
+        Arrays.stream(ar).forEach(e -> System.out.print(e + " "));
     }
 
-    public static void mergeSort(int ar[], int p, int r){
-        if (p<r) {
-            int q = (p+r)/2;
-            mergeSort(ar, p, q);
-            mergeSort(ar, q+1, r);
-            merge(ar,p,q,r);
+    public static void mergeSort(int ar[], int p, int r, int[] temp) {
+        if (p < r) {
+            int q = p + (r - p) / 2;
+            mergeSort(ar, p, q, temp);
+            mergeSort(ar, q + 1, r, temp);
+            merge(ar, temp, p, q, r);
         }
         //return ar;
     }
 
-    public static void merge(int ar[], int p, int q, int r){
+    public static void merge(int ar[], int[] temp, int p, int q, int r) {
 
-        int n1 = q-p+1;
-        int n2 = r - (q+1) + 1;
-        int[] left = new int[n1+1];
-        int[] right = new int [n2+1];
+        for (int i = 0; i <= r; i++) {
+            temp[i]=ar[i];
+        }
 
-        for (int i = 0; i < n1 ;i++) {
-            left [i] = ar[p+i];
+        int i = p;
+        int j = q+ 1;
+        int k = p;
+
+        while (i<=q && j <=r) {
+            if (temp[i]<temp[j]) {
+                ar[k]=temp[i];
+                i++;
+            }else {
+                ar[k]=temp[j];
+                j++;
+            }
+            k++;
         }
-        for (int j = 0; j < n2 ;j++) {
-            right [j] = ar[(q+1)+j];
+        while (i<=q) {
+            ar[k++]=temp[i++];
         }
-        left[n1] = Integer.MAX_VALUE;
-        right[n2] = Integer.MAX_VALUE;
-        int i = 0;
-        int j = 0;
-        for (int k = p; k <= r ; k++) {
-            if (left[i] < right[j])
-                ar[k] = left[i++];
-            else
-                ar[k] = right[j++];
+        while (j<=r) {
+            ar[k++]=temp[j++];
         }
-    }    
+
+    }
 }
